f4f5430e801883fc4b8a15e9450164f619359790
RaeGyu <wogns9769@gmail.com>
2024-06-03 19:55:17 +0900

    +Add
    일시정지버튼 추가

--- Assets/06_Sprites/ButtonControl.cs
+++ Assets/06_Sprites/ButtonControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonControl : MonoBehaviour
 {
@@ -8,5 +9,13 @@ public class ButtonControl : MonoBehaviour
 
     public void OpenSetting(){
         settingPanel.SetActive(!settingPanel.activeInHierarchy);
+    }
+    public void PlayAndStop(Text text){
+        if(Time.timeScale>0)
+            Time.timeScale=0;
+        else
+            Time.timeScale=1;
+
+        text.text = Time.timeScale > 0 ? "일시정지" : "재생";
     }
 }
---
+++ Assets/06_Sprites/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance{
+        get{
+            if(_instance == null){
+                _instance = new GameObject("GameManager").AddComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+    void Awake(){
+        if(_instance != null)
+        {
+            if(_instance != this)
+                Destroy(gameObject);
+        }else{
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
