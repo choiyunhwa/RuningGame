92499762086f71d170e9deee5ed462bb72843ddf
choiyunhwa <sjo4208@naver.com>
2024-06-07 14:17:27 +0900

    Fix > FollowPlayer

--- Assets/02_Scripts/Player/FollowPlayer.cs
+++ Assets/02_Scripts/Player/FollowPlayer.cs
@@ -5,25 +5,40 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public float speed = 5f; 
-
-    private Transform playerTransform; 
+    public Transform playerTransform;
+    public float offsetRange = 2f;
+    private bool isMove = false;
     void Start()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player"); //Fix
-        if (player != null)
-        {
-            playerTransform = player.transform;
-        }
+        //if (playerTransform != null)
+        //{
+        //    MoveToPlayer();
+        //}
     }
 
-    void Update()
+    private void Update()
     {
         if (playerTransform != null)
         {
-            Vector3 targetPosition = playerTransform.position;
-            Vector3 newPosition = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
-            transform.position = newPosition;
+            Vector3 randomOffset = new Vector3(
+                Random.Range(-offsetRange, offsetRange),
+                0,
+                Random.Range(-offsetRange, offsetRange)
+            );
+
+            MoveToPlayer();
         }
     }
 
+
+    private void MoveToPlayer()
+    {
+        if(!isMove)
+        {
+            Debug.Log("¿Ãµø!");
+            Vector3 newPos = Vector3.MoveTowards(transform.position, playerTransform.transform.position, speed);
+            transform.position = newPos;
+            isMove = true;
+        }
+    }
 }
