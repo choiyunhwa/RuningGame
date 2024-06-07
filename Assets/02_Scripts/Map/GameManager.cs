0411793ce77afbefdf9bc13b4d2c8146e82ba472
survival8764 <82863756+survival8764@users.noreply.github.com>
2024-06-07 20:31:44 +0900

    FIX

--- Assets/02_Scripts/Map/GameManager.cs
+++ Assets/02_Scripts/Map/GameManager.cs
@@ -5,7 +5,6 @@ public class GameManager : MonoBehaviour
     public GameObject[] enemyObject;
     public Transform[] spawnPoints;
     public ObjectManager objectManager;
-    public StageData stageData;
 
     public float maxSpawnDelay;
     public float curSpawnDelay;
@@ -13,7 +12,7 @@ public class GameManager : MonoBehaviour
 
     private void Update() 
     {
-        if(curEnemyCnt < stageData.monsterCount)
+        if(curEnemyCnt < GameManager_CH.Instance.stageData.monsterCount)
         {
             curSpawnDelay += Time.deltaTime;
 
@@ -25,7 +24,7 @@ public class GameManager : MonoBehaviour
                 curSpawnDelay = 0;
             }
         }
-        else if(curEnemyCnt == stageData.monsterCount) // 추후에 일반몬스터를 다 없애고 나오는걸로 변경
+        else if(curEnemyCnt == GameManager_CH.Instance.stageData.monsterCount) // 추후에 일반몬스터를 다 없애고 나오는걸로 변경
         {
             SpawnEnemy();
             curEnemyCnt++;
@@ -38,7 +37,7 @@ public class GameManager : MonoBehaviour
         int ranEnemy = Random.Range(0, 3);
         
         // 보스몬스터 추가
-        if(curEnemyCnt == stageData.monsterCount)
+        if(curEnemyCnt == GameManager_CH.Instance.stageData.monsterCount)
         {
             ranEnemy = 3;
         }
